8785ab0fed27816fa9927b2d65b04f39d08a3f08
Neon <10284121+neonspectra@users.noreply.github.com>
2026-01-14 21:16:06 +0000

    Revise welcome text and tool description in Welcome.vue

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -2,8 +2,8 @@
 <!--    <div id="popup">-->
     <div class="game-box uk-padding-remove">
     <article class="game-box uk-article">
-        <h1 class="uk-article-title">Welcome to the Eclipse Phase Character Creator</h1>
-        <p class="uk-article-meta">A website for players of the <a href="http://eclipsephase.com/">Eclipse Phase Role Playing Game.</a></p>
+        <h1 class="uk-article-title">Welcome to EPCC, the Eclipse Phase 1E Character Creator</h1>
+        <p class="uk-article-meta">A chatacter creation and management tool for <a href="http://eclipsephase.com/">Eclipse Phase (First Edition)</a></p>
 <!--    TODO:  This is the welcome page for people who don't have a creator already set up-->
 <!--    TODO:  Better Styling-->
 <!--    People should be able to create a new character, or load an old one from this page.-->
@@ -23,23 +23,21 @@
         <div class="uk-card uk-card-body uk-card-default uk-width-1-3@m game-style">
             <h3 class="uk-card title">What is this?</h3>
             <p>
-                While Eclipse Phase is a fun game to play, character creation can be complicated, and involves large amounts of math.
-                Making matters worse, until the second version of the game is released, each Morph adds even more math.
+                This tool allows you to create and export character sheets for use with the <a href="http://eclipsephase.com/">Eclipse Phase First Edition (1E)</a> tabletop roleplaying game.
+                Stats are automatically calculated based on appropriate Ego and Morph aptitudes so that you can focus on just playing the game.
             </p>
             <p>
-                This website solves those challenges by showing almost all options in a (relatively, I'm still working on it) easy to use interface.
-                One which does all the math for you.
+                This website is not endorsed by or affiliated with <a href="https://posthuman.shop/">Posthuman Studios</a> in any way. 
+                Although Eclipse Phase 1E is now out of print, please consider supporting its creators by purchasing digital copies of the official rulebooks, supplements, or other merch.
             </p>
         </div>
         </div>
 
         <p>Please submit all suggestions and bug reports to the GitHub <a href="https://github.com/neonspectra/epcc/issues" target="_blank">Issues</a> page.</p>
-        <p>If your session has expired and you were still working on your character feel free to open an issue.  I have ideas on how to fix it, but have prioritized fixing major bugs and modernizing the code.</p>
 
-        <h3>Mandatory EU Notice:</h3>
+        <p class="disclaimer"><b>Mandatory EU Notice:</b></p>
         <p class="disclaimer">
             This site uses JavaScript and cookies to function.  By continuing to use this site, you consent to the following privacy policy.
-            In addition, automated bug reporting and Google Analytics are used to create a better site experience, and to reassure myself that all my work was not wasted.
             The creators of this site do not sell your data to anyone.
             This policy may be updated at any time.
         </p>
