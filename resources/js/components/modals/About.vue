278a233b784e9cbb3f4490a66b871cedd3c181c8
Neon <10284121+neonspectra@users.noreply.github.com>
2026-01-14 21:36:15 +0000

    Revise About.vue content and licensing details

--- resources/js/components/modals/About.vue
+++ resources/js/components/modals/About.vue
@@ -5,39 +5,49 @@
             <a href="https://github.com/neonspectra/epcc"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
             <div class="uk-text-center">
                 <h1><b><u> About</u></b></h1>
-                <p>Eclipse Phase Character Creator ({{versionName}})</p>
+                <p>EPCC ({{versionName}})</p>
                 <p>Version {{version}} ({{releaseDate}})</p>
                 <p>Commit {{commitLabel}}</p>
-                <p>A character creator for the <a href="http://eclipsephase.com" target="_blank">Eclipse Phase</a>
-                    role-playing game.</p>
+                <p>A character creation and management tool for <a href="http://eclipsephase.com" target="_blank">Eclipse Phase (First Edition)</a>.</p>
                 <p>
                     Please submit all suggestions and bug reports to the
                     <a href="https://github.com/neonspectra/epcc/issues" target="_blank">Issues</a>
                     page.
                 </p>
                 <p>
-                    Created by:
-                    <b>Russell Bewley,</b>
-                    <b>Stoo Goof,</b>
-                    <b>Derek Payne,</b>
-                    <b>Cédric Reinhardt,</b>
-                    <b>Jigé Pont,</b>
-                    <b>Olivier Murith,</b>
-                    <b>Arthur Moore,</b>
+                    Maintained by:
                     <b>neon@neosynth.net</b>
                 </p>
-                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US" target="_blank">
+                <p>
+                    Original created by:
+                    <b>Russell Bewley, </b>
+                    <b>Stoo Goof, </b>
+                    <b>Derek Payne, </b>
+                    <b>Cédric Reinhardt, </b>
+                    <b>Jigé Pont, </b>
+                    <b>Olivier Murith, </b>
+                    <b>Arthur Moore</b>
+                </p>
+                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">
                     <img alt="Creative Commons License" style="border-width:0"
-                         src="https://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png"/>
+                         src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"/>
                 </a>
                 <p>
                     <small>
                         This work is licensed under a
-                        <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"
+                        <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
                            target="_blank">
-                            Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unsupported License
+                            Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
                         </a>
                         .
+                    </small>
+                    <small>
+                        This work is a derivative work based on <a href="https://github.com/Eclipse-Phase-Unofficial/ep-character-creator">previous work by Arthur Moore and other authors</a> licensed under a
+                        <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"
+                           target="_blank">
+                            Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+                        </a>
+                        . Changes were made.
                     </small>
                 </p>
             </div>
